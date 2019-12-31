03146261117415d4db2eca7b71df01dfce2a3eb4
Charutha Bandara <charuthabandara@gmail.com>
2019-12-31 14:48:09 -0500

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var passport = require('passport')
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors')
 require('dotenv').config();
 
 var productsRouter = require('./routes/products');
@@ -14,6 +15,7 @@ var app = express();
 
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
